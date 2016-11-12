f2952fc6209549baf65fb3126a967cba5b2cdd76
Anton Rufino <anton.vongola@gmail.com>
2016-11-12 12:51:11 +0800

    Created lexeme.js

---
+++ js/lexeme.js
@@ -0,0 +1,13 @@
+export const lexemeRegEx = {
+    HAI: {tokenRegEx: /HAI/, classification: 'Code delimiter'}},
+    KTHXBYE: {tokenRegEx: /KTHXBYE/, classification: 'Code delimiter'}},
+    NEW_LINE: {tokenRegEx: /\n/, classification: 'Statement delimiter'}},
+    I_HAS_A: {tokenRegEx: /I HAS A/, classification: 'Variable declaration delimiter'}},
+    ITZ: {tokenRegEx: /ITZ/, classification: 'Variable initialization'}},
+    R: {tokenRegEx: /R/, classification: 'Variable assignment'}},
+    BTW: {tokenRegEx: /BTW/, classification: 'Comment delimiter'}},
+    VISIBLE: {tokenRegEx: /VISIBLE/, classification: 'Output keyword'}},
+    GIMMEH: {tokenRegEx: /GIMMEH/, classification: 'Input keyword'}},
+    WIN: {tokenRegEx: /WIN/, classification: 'TROOF literal'}},
+    FAIL: {tokenRegEx: /FAIL/, classification: 'TROOF literal'}}
+}
