6ee7c482a1f0a73b5c8e6fcd67df869777dcbc37
Anton Rufino <anton.vongola@gmail.com>
2016-11-16 21:27:33 +0800

    Create skeleton for parser.js

---
+++ js/parser.js
@@ -0,0 +1,16 @@
+(() => {
+    angular.module('app')
+    .factory('parser', () => {
+        return (lexemeTable, symbolTable) => {
+            let parser = {}
+
+            // Define parser functions here
+
+            parser.analyze = () => {
+
+            }
+
+            return parser;
+        }
+    });
+})();
