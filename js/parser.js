9da5cfafdaa461c6ea0960f907d91e69910f8619
Anton Rufino <anton.vongola@gmail.com>
2016-11-30 14:07:34 +0800

    fix shit

--- js/parser.js
+++ js/parser.js
@@ -3,6 +3,7 @@
     .factory('parser', () => {
         function arithmetic(lexemeTable, symbolTable, i) {
             let expr = [];
+            let hasFloat = false;
 
             while (i < lexemeTable.length) {
                 if (lexemeTable[i].tokenType === 'INTEGER_LITERAL') {
@@ -13,6 +14,8 @@
                     expr.push(parseFloat(lexemeTable[i].token))
                     if (i + 1 >= lexemeTable.length ||
                         lexemeTable[i + 1].tokenType !== 'AN') break;
+
+                    hasFloat = true;
                 } else if (lexemeTable[i].tokenType === 'ARITHMETIC_OPERATOR') {
                     expr.push(lexemeTable[i].token);
                 } else if (lexemeTable[i].tokenType === 'AN') {
@@ -31,11 +34,19 @@
                                 } else {
                                     expr.push(result);
                                 }
+
+                                hasFloat = true;
                             } else {
                                 expr.push(result);
+                                if (descriptor.value.indexOf('.') !== -1) {
+                                    hasFloat = true;
+                                }
                             }
                         } else {
                             expr.push(descriptor.value);
+                            if (descriptor.dataType === 'NUMBAR') {
+                                hasFloat = true;
+                            }
                         }
                     }
                     else {
@@ -56,9 +67,13 @@
                             return {value: null, index: -1};
                         } else {
                             expr.push(result);
+                            hasFloat = true;
                         }
                     } else {
                         expr.push(result);
+                        if (lexemeTable[i].token.indexOf('.') !== -1) {
+                            hasFloat = true;
+                        }
                     }
 
                     i += 1;
@@ -115,20 +130,20 @@
                 return {value: null, index: -1};
             }
 
-            return {value: stack.pop(), index: i};
+            return {value: stack.pop(), index: i, dataType: hasFloat ? "NUMBAR" : "NUMBR"};
         }
 
         function initialize(lexemeTable, symbolTable, index) {
             if(lexemeTable[index+1].tokenType==='IDENTIFIER'){
                 if(lexemeTable[index+2].tokenType==='ITZ'){
                     if(lexemeTable[index+3].tokenType==='INTEGER_LITERAL'){
-                        symbolTable[lexemeTable[index+1].token] = { value: parseInt(lexemeTable[index+3].token), dataType: lexemeTable[index+3].tokenType};
+                        symbolTable[lexemeTable[index+1].token] = { value: parseInt(lexemeTable[index+3].token), dataType: 'NUMBR'};
                     } else if (lexemeTable[index+3].tokenType==='FLOATING_POINT_LITERAL') {
-                        symbolTable[lexemeTable[index+1].token] = { value: parseFloat(lexemeTable[index+3].token), dataType: lexemeTable[index+3].tokenType};
+                        symbolTable[lexemeTable[index+1].token] = { value: parseFloat(lexemeTable[index+3].token), dataType: 'NUMBAR'};
                     } else if(lexemeTable[index+3].tokenType==='STRING_DELIMITER'){
-                        symbolTable[lexemeTable[index+1].token] = { value: lexemeTable[index+4].token, dataType: lexemeTable[index+4].tokenType};
+                        symbolTable[lexemeTable[index+1].token] = { value: lexemeTable[index+4].token, dataType: 'YARN'};
                     } else if (lexemeTable[index+3].tokenType==='WIN' || lexemeTable[index+3].tokenType==='FAIL') {
-                        symbolTable[lexemeTable[index+1].token] = { value: lexemeTable[index+3].token, dataType: lexemeTable[index+3].tokenType};
+                        symbolTable[lexemeTable[index+1].token] = { value: lexemeTable[index+3].token, dataType: 'TROOF'};
                     } else if(lexemeTable[index+3].tokenType==='IDENTIFIER'){
                         if(symbolTable[lexemeTable[index+3].token] !== undefined){
                             symbolTable[lexemeTable[index+1].token] = { value: symbolTable[lexemeTable[index+3].token].value, dataType: symbolTable[lexemeTable[index+3].dataType]};
@@ -137,7 +152,7 @@
                         let result = arithmetic(lexemeTable, symbolTable, index+3);
                         let newIndex = result.index;
                         if (newIndex!==-1) {
-                            symbolTable[lexemeTable[index+1].token] = { value: result.value, dataType: symbolTable[lexemeTable[index+3].dataType]};
+                            symbolTable[lexemeTable[index+1].token] = { value: result.value, dataType: result.dataType};
                             return newIndex;
                         } else {
                             // TODO: error
@@ -147,7 +162,7 @@
                         let result = smoosh(lexemeTable, symbolTable, index+3);
                         let newIndex = result.index;
                         if (newIndex!==-1) {
-                            symbolTable[lexemeTable[index+1].token] = { value: result.value, dataType: symbolTable[lexemeTable[index+3].dataType]};
+                            symbolTable[lexemeTable[index+1].token] = { value: result.value, dataType: 'YARN'};
                             return newIndex;
                         } else {
                             // TODO: error
@@ -193,6 +208,10 @@
                         }
                         else {
                             str = str + symbolTable[lexemeTable[i].token].value;
+                            if (symbolTable[lexemeTable[i].token].dataType == 'NUMBAR' &&
+                                Number.isInteger(symbolTable[lexemeTable[i].token].value)) {
+                                str = str + '.0';
+                            }
                             i+=1;
                         }
                     }
@@ -206,7 +225,12 @@
         // Sobra kulang pa nito. Identifier lang prinaprocess.
         function visible(lexemeTable, symbolTable, $scope, index) {
             if(lexemeTable[index+1].tokenType==='IDENTIFIER'){
-                $scope.terminal = $scope.terminal + symbolTable[lexemeTable[index+1].token].value + '\n';
+                $scope.terminal = $scope.terminal + symbolTable[lexemeTable[index+1].token].value;
+                if (symbolTable[lexemeTable[index+1].token].dataType == 'NUMBAR' &&
+                    Number.isInteger(symbolTable[lexemeTable[index+1].token].value)) {
+                    $scope.terminal = $scope.terminal + '.0';
+                }
+                $scope.terminal = $scope.terminal + '\n';
             }
             else if (lexemeTable[index+1].tokenType==='INTEGER_LITERAL') {
                 $scope.terminal = $scope.terminal + lexemeTable[index+1].token + '\n';
@@ -237,18 +261,18 @@
         function assign(lexemeTable, symbolTable, index) {
              if(lexemeTable[index-1].tokenType==='IDENTIFIER'){
                 if(lexemeTable[index+1].tokenType==='INTEGER_LITERAL'){
-                    symbolTable[lexemeTable[index-1].token] = { value: parseInt(lexemeTable[index+1].token), dataType: lexemeTable[index+1].tokenType};
+                    symbolTable[lexemeTable[index-1].token] = { value: parseInt(lexemeTable[index+1].token), dataType: 'NUMBR'};
                 } else if (lexemeTable[index+1].tokenType==='FLOATING_POINT_LITERAL') {
-                    symbolTable[lexemeTable[index-1].token] = { value: parseFloat(lexemeTable[index+1].token), dataType: lexemeTable[index+1].tokenType};
+                    symbolTable[lexemeTable[index-1].token] = { value: parseFloat(lexemeTable[index+1].token), dataType: 'NUMBAR'};
                 } else if(lexemeTable[index+2].tokenType==='STRING_LITERAL'){
-                    symbolTable[lexemeTable[index-1].token] = { value: lexemeTable[index+2].token, dataType: lexemeTable[index+2].tokenType};
+                    symbolTable[lexemeTable[index-1].token] = { value: lexemeTable[index+2].token, dataType: 'YARN'};
                 } else if (lexemeTable[index+1].tokenType==='WIN' || lexemeTable[index+1].tokenType==='FAIL') {
-                    symbolTable[lexemeTable[index-1].token] = { value: lexemeTable[index+1].token, dataType: lexemeTable[index+1].tokenType};
+                    symbolTable[lexemeTable[index-1].token] = { value: lexemeTable[index+1].token, dataType: 'TROOF'};
                 } else if (lexemeTable[index+1].tokenType==='IDENTIFIER') {
                     symbolTable[lexemeTable[index-1].token] = { value: symbolTable[lexemeTable[index+1].token].value, dataType: symbolTable[lexemeTable[index+1].token].dataType};
                 } else if (lexemeTable[index+1].tokenType==='ARITHMETIC_OPERATOR') {
                     let result = arithmetic(lexemeTable, symbolTable, index+1);
-                    symbolTable[lexemeTable[index-1].token] = { value: result.value, dataType: 'NUMBR'};
+                    symbolTable[lexemeTable[index-1].token] = { value: result.value, dataType: result.dataType};
                     return result.index;
                 } else if (lexemeTable[index+1].tokenType==='SMOOSH') {
                     let result = smoosh(lexemeTable, symbolTable, index+1);
@@ -273,7 +297,72 @@
             return {pendingVar: null, index: -1};
         }
 
-        return (lexemeTable, symbolTable, start, $scope) => {
+        function wtf(lexemeTable, symbolTable, i, $scope) {
+            i += 1;
+            while (i < lexemeTable.length) {
+                if (lexemeTable[i].tokenType === 'OMG') {
+                    let caseValue;
+                    let start;
+
+                    if (lexemeTable[i+1].tokenType === 'INTEGER_LITERAL') {//determines the case values
+                        caseValue = parseInt(lexemeTable[i+1].token);
+                        start = i+1;
+                    } else if (lexemeTable[i+1].tokenType === 'FLOATING_POINT_LITERAL') {
+                        caseValue = parseFloat(lexemeTable[i+1].token);
+                        start = i+1;
+                    } else if (lexemeTable[i+2].tokenType === 'STRING_LITERAL') {
+                        caseValue = lexemeTable[i+2].token;
+                        start = i+3;
+                    } else if (lexemeTable[i+1].tokenType === 'WIN') {
+                        caseValue = lexemeTable[i+1].token;
+                        start = i+1;
+                    } else if (lexemeTable[i+1].tokenType === 'FAIL') {
+                        caseValue = lexemeTable[i+1].token;
+                        start = i+1;
+                    }
+
+                    if (symbolTable['IT'].value === caseValue) {
+                        console.log(symbolTable['IT'].value);
+                        console.log(caseValue);
+                        return start;
+                    } else {
+                        i += 1;
+                    }
+                } else if (lexemeTable[i].tokenType === 'OMGWTF') {
+                    return i;
+                } else if (lexemeTable[i].tokenType === 'OIC') {
+                    return i;
+                } else {
+                    i += 1;
+                }
+            }
+        }
+        function if_else(lexemeTable, symbolTable, i) {
+            if(symbolTable['IT'].value === 'WIN'){
+                console.log('Win Detected');
+                while(lexemeTable[i].tokenType !== 'YA_RLY'){
+                    if(lexemeTable[i].tokenType === 'NO_WAI') {
+                        return i;
+                    }
+                     i +=1;
+                     console.log(i);
+                }
+                return i;
+            } else if(symbolTable['IT'].value === 'FAIL'){
+                console.log('Fail Detected');
+               while(lexemeTable[i].tokenType !== 'NO_WAI'){                    
+                    if (lexemeTable[i].tokenType === 'OIC') {
+                        return i;
+                    }
+                    i += 1;
+                }
+                return i;
+                
+            }
+            
+        }
+
+        function parse(lexemeTable, symbolTable, start, $scope) {
             for (let i = start; i < lexemeTable.length; i++) {
                 if (lexemeTable[i].tokenType === 'I_HAS_A') {
                     i = initialize(lexemeTable, symbolTable, i);
@@ -290,7 +379,7 @@
                     let result = arithmetic(lexemeTable, symbolTable, i);
 
                     if (result.index !== -1)
-                        symbolTable['IT'] = {value: result.value, dataType: "temp"};
+                        symbolTable['IT'] = {value: result.value, dataType: result.dataType};
 
                     i = result.index;
                 }
@@ -308,6 +397,21 @@
                         $('#enter').attr('disabled', false);
                         return result;
                     }
+                } else if (lexemeTable[i].tokenType === 'O_RLY?'){
+                    i = if_else(lexemeTable, symbolTable, i);
+
+                }  else if (lexemeTable[i].tokenType === 'NO_WAI') {
+                    for (; i < lexemeTable.length; ++i) {
+                        if (lexemeTable[i].tokenType === 'OIC') break
+                    }
+                } else if (lexemeTable[i].tokenType === 'WTF?') {
+                    i = wtf(lexemeTable, symbolTable, i, $scope);
+                } else if (lexemeTable[i].tokenType === 'GTFO') {
+                    for (; i < lexemeTable.length; ++i) {
+                        if (lexemeTable[i].tokenType === 'OIC') break
+                    }
+                } else if (lexemeTable[i].tokenType === 'OIC') {
+                    continue;
                 }
 
                 if (i === -1) {
@@ -320,5 +424,7 @@
 
             return null;
         }
+
+        return parse;
     });
 })();
